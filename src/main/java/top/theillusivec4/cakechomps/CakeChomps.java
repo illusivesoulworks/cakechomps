ca32454a6bc54d62105e4b703cf9f32ffbfb843b
TheIllusiveC4 <itwasmec4@gmail.com>
2019-12-11 22:53:07 -0800

    small fixes

--- src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
+++ src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
@@ -44,24 +44,27 @@ public class CakeChomps implements ModInitializer {
 
       if (block instanceof CakeBlock && player.canConsume(false)) {
         Item item = Item.BLOCK_ITEMS.get(block);
-        ItemStack stack = new ItemStack(item);
 
-        for (int i = 0; i < 16; i++) {
-          Vec3d vec3d = new Vec3d(((double) RAND.nextFloat() - 0.5D) * 0.1D,
-              RAND.nextDouble() * 0.1D + 0.1D, 0.0D);
-          vec3d = vec3d.rotateX(-player.pitch * 0.017453292F);
-          vec3d = vec3d.rotateY(-player.yaw * 0.017453292F);
-          double d0 = (double) (-RAND.nextFloat()) * 0.6D - 0.3D;
-          Vec3d vec3d1 = new Vec3d(((double) RAND.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
-          vec3d1 = vec3d1.rotateX(-player.pitch * 0.017453292F);
-          vec3d1 = vec3d1.rotateY(-player.yaw * 0.017453292F);
-          vec3d1 = vec3d1.add(player.getX(), player.getEyeY(), player.getZ());
-          world.addParticle(new ItemStackParticleEffect(ParticleTypes.ITEM, stack), vec3d1.x,
-              vec3d1.y, vec3d1.z, vec3d.x, vec3d.y + 0.05D, vec3d.z);
+        if (item != null) {
+          ItemStack stack = new ItemStack(item);
+
+          for (int i = 0; i < 16; i++) {
+            Vec3d vec3d = new Vec3d(((double) RAND.nextFloat() - 0.5D) * 0.1D,
+                RAND.nextDouble() * 0.1D + 0.1D, 0.0D);
+            vec3d = vec3d.rotateX(-player.pitch * 0.017453292F);
+            vec3d = vec3d.rotateY(-player.yaw * 0.017453292F);
+            double d0 = (double) (-RAND.nextFloat()) * 0.6D - 0.3D;
+            Vec3d vec3d1 = new Vec3d(((double) RAND.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
+            vec3d1 = vec3d1.rotateX(-player.pitch * 0.017453292F);
+            vec3d1 = vec3d1.rotateY(-player.yaw * 0.017453292F);
+            vec3d1 = vec3d1.add(player.getX(), player.getEyeY(), player.getZ());
+            world.addParticle(new ItemStackParticleEffect(ParticleTypes.ITEM, stack), vec3d1.x,
+                vec3d1.y, vec3d1.z, vec3d.x, vec3d.y + 0.05D, vec3d.z);
+          }
+
+          player.playSound(player.getEatSound(stack), 0.5F + 0.5F * (float) RAND.nextInt(2),
+              (RAND.nextFloat() - RAND.nextFloat()) * 0.2F + 1F);
         }
-
-        player.playSound(player.getEatSound(stack), 0.5F + 0.5F * (float) RAND.nextInt(2),
-            (RAND.nextFloat() - RAND.nextFloat()) * 0.2F + 1F);
       }
 
       return ActionResult.PASS;
