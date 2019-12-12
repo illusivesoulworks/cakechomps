04329c40fdc7f2cfcc575d58df5c0aef3d7b22a0
TheIllusiveC4 <itwasmec4@gmail.com>
2019-12-11 20:59:53 -0800

    finish 1.15 port

--- src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
+++ src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
@@ -19,6 +19,7 @@
 
 package top.theillusivec4.cakechomps;
 
+import java.util.Random;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.Block;
@@ -27,44 +28,43 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ItemStackParticleEffect;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
-import java.util.Random;
-
 public class CakeChomps implements ModInitializer {
 
-    private static final Random RAND = new Random();
+  private static final Random RAND = new Random();
 
-    @Override
-    public void onInitialize() {
-        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-            BlockPos pos = hitResult.getBlockPos();
-            Block block = world.getBlockState(pos).getBlock();
+  @Override
+  public void onInitialize() {
+    UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+      BlockPos pos = hitResult.getBlockPos();
+      Block block = world.getBlockState(pos).getBlock();
 
-            if (block instanceof CakeBlock && player.canConsume(false)) {
-                Item item = Item.BLOCK_ITEMS.get(block);
-                ItemStack stack = new ItemStack(item);
+      if (block instanceof CakeBlock && player.canConsume(false)) {
+        Item item = Item.BLOCK_ITEMS.get(block);
+        ItemStack stack = new ItemStack(item);
 
-                for (int i = 0; i < 5; i++) {
-                    Vec3d vec3d = new Vec3d(((double)RAND.nextFloat() - 0.5D) * 0.1D, RAND.nextDouble() * 0.1D + 0.1D, 0.0D);
-                    vec3d = vec3d.rotateX(-player.pitch * 0.017453292F);
-                    vec3d = vec3d.rotateY(-player.yaw * 0.017453292F);
-                    double d0 = (double)(-RAND.nextFloat()) * 0.6D - 0.3D;
-                    Vec3d vec3d1 = new Vec3d(((double)RAND.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
-                    vec3d1 = vec3d1.rotateX(-player.pitch * 0.017453292F);
-                    vec3d1 = vec3d1.rotateY(-player.yaw * 0.017453292F);
-                    vec3d1 = vec3d1.add(player.getX(), player.getY() + (double)player.getStandingEyeHeight(), player.getZ());
-                    world.addParticle(new ItemStackParticleEffect(ParticleTypes.ITEM, stack), vec3d1.x, vec3d1.y, vec3d1.z,
-                            vec3d.x, vec3d.y + 0.05D, vec3d.z);
-                }
-                world.playSound(null, pos, SoundEvents.ENTITY_GENERIC_EAT, SoundCategory.BLOCKS, 0.5F + 0.5F *
-                        (float)RAND.nextInt(2), (RAND.nextFloat() - RAND.nextFloat()) * 0.2F + 1F);
-            }
-            return ActionResult.PASS;
-        });
-    }
+        for (int i = 0; i < 16; i++) {
+          Vec3d vec3d = new Vec3d(((double) RAND.nextFloat() - 0.5D) * 0.1D,
+              RAND.nextDouble() * 0.1D + 0.1D, 0.0D);
+          vec3d = vec3d.rotateX(-player.pitch * 0.017453292F);
+          vec3d = vec3d.rotateY(-player.yaw * 0.017453292F);
+          double d0 = (double) (-RAND.nextFloat()) * 0.6D - 0.3D;
+          Vec3d vec3d1 = new Vec3d(((double) RAND.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
+          vec3d1 = vec3d1.rotateX(-player.pitch * 0.017453292F);
+          vec3d1 = vec3d1.rotateY(-player.yaw * 0.017453292F);
+          vec3d1 = vec3d1.add(player.getX(), player.getEyeY(), player.getZ());
+          world.addParticle(new ItemStackParticleEffect(ParticleTypes.ITEM, stack), vec3d1.x,
+              vec3d1.y, vec3d1.z, vec3d.x, vec3d.y + 0.05D, vec3d.z);
+        }
+
+        player.playSound(player.getEatSound(stack), 0.5F + 0.5F * (float) RAND.nextInt(2),
+            (RAND.nextFloat() - RAND.nextFloat()) * 0.2F + 1F);
+      }
+
+      return ActionResult.PASS;
+    });
+  }
 }
