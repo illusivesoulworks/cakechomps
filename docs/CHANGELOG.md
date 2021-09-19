68176245c58c9648c79d2eec89d3ce7941df84a4
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-09-18 22:46:22 -0700

    backport 1.17 cake effect fix

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project does not adhere to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 This project uses MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
 
+### [1.16.5-5.0.0.3] - 2021.09.18
+### Fixed
+- Fixed cake effects activating even when cakes are not consumed/activated [#2](https://github.com/TheIllusiveC4/CakeChomps/issues/2)
+
 ### [1.16.4-5.0.0.2] - 2020.11.15
 ### Changed
 - Updated to 1.16.4
--- src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
+++ src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
@@ -25,9 +25,13 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.CakeBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.particles.ItemParticleData;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
@@ -56,37 +60,57 @@ public class CakeChomps {
     World world = evt.getWorld();
     PlayerEntity player = evt.getPlayer();
     BlockPos pos = evt.getPos();
+    BlockRayTraceResult rayTraceResult = evt.getHitVec();
+    Hand hand = evt.getHand();
     BlockState state = world.getBlockState(pos);
     Block block = state.getBlock();
 
     if (!(block instanceof CakeBlock) || !player.canEat(false)) {
       return;
     }
-    ItemStack stack = block.getPickBlock(state, null, world, pos, player);
+    ItemUseContext itemusecontext = new ItemUseContext(player, hand, rayTraceResult);
+    ItemStack stack = player.getHeldItem(hand);
 
-    for (int i = 0; i < 16; ++i) {
-      Vector3d vec3d = new Vector3d(((double) RAND.nextFloat() - 0.5D) * 0.1D,
-          Math.random() * 0.1D + 0.1D, 0.0D);
-      vec3d = vec3d.rotatePitch(-player.rotationPitch * ((float) Math.PI / 180F));
-      vec3d = vec3d.rotateYaw(-player.rotationYaw * ((float) Math.PI / 180F));
-      double d0 = (double) (-RAND.nextFloat()) * 0.6D - 0.3D;
-      Vector3d vec3d1 = new Vector3d(((double) RAND.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
-      vec3d1 = vec3d1.rotatePitch(-player.rotationPitch * ((float) Math.PI / 180F));
-      vec3d1 = vec3d1.rotateYaw(-player.rotationYaw * ((float) Math.PI / 180F));
-      vec3d1 = vec3d1.add(player.getPosX(), player.getPosYEye(), player.getPosZ());
-      ItemParticleData particle = new ItemParticleData(ParticleTypes.ITEM, stack);
+    if (evt.getUseItem() != net.minecraftforge.eventbus.api.Event.Result.DENY) {
+      ActionResultType result = stack.onItemUseFirst(itemusecontext);
 
-      if (player.world instanceof ServerWorld) {
-        ServerWorld serverWorld = (ServerWorld) player.world;
-        serverWorld
-            .spawnParticle(particle, vec3d1.x, vec3d1.y, vec3d1.z, 1, vec3d.x, vec3d.y + 0.05D,
-                vec3d.z, 0.0D);
-      } else {
-        world
-            .addParticle(particle, vec3d1.x, vec3d1.y, vec3d1.z, vec3d.x, vec3d.y + 0.05D, vec3d.z);
+      if (result != ActionResultType.PASS) {
+        return;
       }
     }
-    player.playSound(player.getEatSound(stack), 0.5F + 0.5F * (float) RAND.nextInt(2),
-        (RAND.nextFloat() - RAND.nextFloat()) * 0.2F + 1.0F);
+    boolean flag =
+        !player.getHeldItemMainhand().isEmpty() || !player.getHeldItemOffhand().isEmpty();
+    boolean flag1 = (player.isSecondaryUseActive() && flag) &&
+        !(player.getHeldItemMainhand().doesSneakBypassUse(world, pos, player) &&
+            player.getHeldItemOffhand().doesSneakBypassUse(world, pos, player));
+
+    if (evt.getUseBlock() == net.minecraftforge.eventbus.api.Event.Result.ALLOW ||
+        (evt.getUseBlock() != net.minecraftforge.eventbus.api.Event.Result.DENY && !flag1)) {
+      ItemStack blockStack = block.getPickBlock(state, null, world, pos, player);
+
+      for (int i = 0; i < 16; ++i) {
+        Vector3d vec3d = new Vector3d(((double) RAND.nextFloat() - 0.5D) * 0.1D,
+            Math.random() * 0.1D + 0.1D, 0.0D);
+        vec3d = vec3d.rotatePitch(-player.rotationPitch * ((float) Math.PI / 180F));
+        vec3d = vec3d.rotateYaw(-player.rotationYaw * ((float) Math.PI / 180F));
+        double d0 = (double) (-RAND.nextFloat()) * 0.6D - 0.3D;
+        Vector3d vec3d1 = new Vector3d(((double) RAND.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
+        vec3d1 = vec3d1.rotatePitch(-player.rotationPitch * ((float) Math.PI / 180F));
+        vec3d1 = vec3d1.rotateYaw(-player.rotationYaw * ((float) Math.PI / 180F));
+        vec3d1 = vec3d1.add(player.getPosX(), player.getPosYEye(), player.getPosZ());
+        ItemParticleData particle = new ItemParticleData(ParticleTypes.ITEM, blockStack);
+
+        if (player.world instanceof ServerWorld) {
+          ServerWorld serverWorld = (ServerWorld) player.world;
+          serverWorld.spawnParticle(particle, vec3d1.x, vec3d1.y, vec3d1.z, 1, vec3d.x,
+              vec3d.y + 0.05D, vec3d.z, 0.0D);
+        } else {
+          world.addParticle(particle, vec3d1.x, vec3d1.y, vec3d1.z, vec3d.x, vec3d.y + 0.05D,
+              vec3d.z);
+        }
+      }
+      player.playSound(player.getEatSound(blockStack), 0.5F + 0.5F * (float) RAND.nextInt(2),
+          (RAND.nextFloat() - RAND.nextFloat()) * 0.2F + 1.0F);
+    }
   }
 }
