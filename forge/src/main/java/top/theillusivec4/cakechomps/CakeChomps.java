b11036436adaecc0fe9c43f302419512a60fb280
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-09-18 22:35:51 -0700

    fix cake effect activation, closes #2

--- forge/src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
+++ forge/src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
@@ -25,12 +25,16 @@ import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CakeBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -57,35 +61,56 @@ public class CakeChomps {
     Level world = evt.getWorld();
     Player player = evt.getPlayer();
     BlockPos pos = evt.getPos();
+    BlockHitResult rayTraceResult = evt.getHitVec();
+    InteractionHand hand = evt.getHand();
     BlockState state = world.getBlockState(pos);
     Block block = state.getBlock();
 
     if (!(block instanceof CakeBlock) || !player.canEat(false)) {
       return;
     }
-    ItemStack stack = block.getPickBlock(state, null, world, pos, player);
+    UseOnContext useoncontext = new UseOnContext(player, hand, rayTraceResult);
+    ItemStack stack = player.getItemInHand(hand);
 
-    for (int i = 0; i < 16; ++i) {
-      Vec3 vec3 =
-          new Vec3(((double) RANDOM.nextFloat() - 0.5D) * 0.1D, Math.random() * 0.1D + 0.1D, 0.0D);
-      vec3 = vec3.xRot(-player.getXRot() * ((float) Math.PI / 180F));
-      vec3 = vec3.yRot(-player.getYRot() * ((float) Math.PI / 180F));
-      double d0 = (double) (-RANDOM.nextFloat()) * 0.6D - 0.3D;
-      Vec3 vec31 = new Vec3(((double) RANDOM.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
-      vec31 = vec31.xRot(-player.getXRot() * ((float) Math.PI / 180F));
-      vec31 = vec31.yRot(-player.getYRot() * ((float) Math.PI / 180F));
-      vec31 = vec31.add(player.getX(), player.getEyeY(), player.getZ());
-      ParticleOptions particle = new ItemParticleOption(ParticleTypes.ITEM, stack);
+    if (evt.getUseItem() != net.minecraftforge.eventbus.api.Event.Result.DENY) {
+      InteractionResult result = stack.onItemUseFirst(useoncontext);
 
-      if (player.level instanceof ServerLevel serverWorld) {
-        serverWorld
-            .sendParticles(particle, vec31.x, vec31.y, vec31.z, 1, vec3.x, vec3.y + 0.05D, vec3.z,
-                0.0D);
-      } else {
-        world.addParticle(particle, vec31.x, vec31.y, vec31.z, vec3.x, vec3.y + 0.05D, vec3.z);
+      if (result != InteractionResult.PASS) {
+        return;
       }
     }
-    player.playSound(player.getEatingSound(stack), 0.5F + 0.5F * (float) RANDOM.nextInt(2),
-        (RANDOM.nextFloat() - RANDOM.nextFloat()) * 0.2F + 1.0F);
+    boolean flag = !player.getMainHandItem().isEmpty() || !player.getOffhandItem().isEmpty();
+    boolean flag1 = (player.isSecondaryUseActive() && flag) &&
+        !(player.getMainHandItem().doesSneakBypassUse(world, pos, player) &&
+            player.getOffhandItem().doesSneakBypassUse(world, pos, player));
+
+    if (evt.getUseBlock() == net.minecraftforge.eventbus.api.Event.Result.ALLOW ||
+        (evt.getUseBlock() != net.minecraftforge.eventbus.api.Event.Result.DENY && !flag1)) {
+      ItemStack blockStack = block.getPickBlock(state, null, world, pos, player);
+
+      for (int i = 0; i < 16; ++i) {
+        Vec3 vec3 =
+            new Vec3(((double) RANDOM.nextFloat() - 0.5D) * 0.1D, Math.random() * 0.1D + 0.1D,
+                0.0D);
+        vec3 = vec3.xRot(-player.getXRot() * ((float) Math.PI / 180F));
+        vec3 = vec3.yRot(-player.getYRot() * ((float) Math.PI / 180F));
+        double d0 = (double) (-RANDOM.nextFloat()) * 0.6D - 0.3D;
+        Vec3 vec31 = new Vec3(((double) RANDOM.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
+        vec31 = vec31.xRot(-player.getXRot() * ((float) Math.PI / 180F));
+        vec31 = vec31.yRot(-player.getYRot() * ((float) Math.PI / 180F));
+        vec31 = vec31.add(player.getX(), player.getEyeY(), player.getZ());
+        ParticleOptions particle = new ItemParticleOption(ParticleTypes.ITEM, blockStack);
+
+        if (player.level instanceof ServerLevel serverWorld) {
+          serverWorld
+              .sendParticles(particle, vec31.x, vec31.y, vec31.z, 1, vec3.x, vec3.y + 0.05D, vec3.z,
+                  0.0D);
+        } else {
+          world.addParticle(particle, vec31.x, vec31.y, vec31.z, vec3.x, vec3.y + 0.05D, vec3.z);
+        }
+      }
+      player.playSound(player.getEatingSound(blockStack), 0.5F + 0.5F * (float) RANDOM.nextInt(2),
+          (RANDOM.nextFloat() - RANDOM.nextFloat()) * 0.2F + 1.0F);
+    }
   }
 }
