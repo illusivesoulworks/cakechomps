9a345454c11ab78eb5a30e8071f02e7db13b1f8b
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2022-03-02 02:05:17 -0800

    fix cake effects when not interacting, closes #3

--- fabric/CHANGELOG.md
+++ fabric/CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.4.1-1.18.2] - 2022.03.02
+### Fixed
+- Fixed cake effects activating even when cakes are not consumed/activated [#3](https://github.com/TheIllusiveC4/CakeChomps/issues/3)
+
 ## [1.4.0-1.18] - 2021.12.09
 ### Changed
 - Updated to Minecraft 1.18
--- fabric/src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
+++ fabric/src/main/java/top/theillusivec4/cakechomps/CakeChomps.java
@@ -23,8 +23,8 @@ import java.util.Random;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.CakeBlock;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ItemStackParticleEffect;
 import net.minecraft.particle.ParticleTypes;
@@ -40,32 +40,31 @@ public class CakeChomps implements ModInitializer {
   public void onInitialize() {
     UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
       BlockPos pos = hitResult.getBlockPos();
-      Block block = world.getBlockState(pos).getBlock();
+      BlockState blockState = world.getBlockState(pos);
+      Block block = blockState.getBlock();
 
-      if (block instanceof CakeBlock && player.canConsume(false)) {
-        Item item = Item.BLOCK_ITEMS.get(block);
-
-        if (item != null) {
-          ItemStack stack = new ItemStack(item);
-          float modifier = 0.017453292F;
-
-          for (int i = 0; i < 16; i++) {
-            Vec3d vec3d = new Vec3d(((double) RANDOM.nextFloat() - 0.5D) * 0.1D,
-                RANDOM.nextDouble() * 0.1D + 0.1D, 0.0D);
-            vec3d = vec3d.rotateX(-player.getPitch() * modifier);
-            vec3d = vec3d.rotateY(-player.getYaw() * modifier);
-            double d0 = (double) (-RANDOM.nextFloat()) * 0.6D - 0.3D;
-            Vec3d vec3d1 = new Vec3d(((double) RANDOM.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
-            vec3d1 = vec3d1.rotateX(-player.getPitch() * modifier);
-            vec3d1 = vec3d1.rotateY(-player.getYaw() * modifier);
-            vec3d1 = vec3d1.add(player.getX(), player.getEyeY(), player.getZ());
-            world.addParticle(new ItemStackParticleEffect(ParticleTypes.ITEM, stack), vec3d1.x,
-                vec3d1.y, vec3d1.z, vec3d.x, vec3d.y + 0.05D, vec3d.z);
-          }
-          player.playSound(player.getEatSound(stack), 0.5F + 0.5F * (float) RANDOM.nextInt(2),
-              (RANDOM.nextFloat() - RANDOM.nextFloat()) * 0.2F + 1F);
-        }
+      if (!(block instanceof CakeBlock) || !player.canConsume(false) ||
+          player.shouldCancelInteraction()) {
+        return ActionResult.PASS;
       }
+      ItemStack stack = block.getPickStack(world, pos, blockState);
+      float modifier = 0.017453292F;
+
+      for (int i = 0; i < 16; i++) {
+        Vec3d vec3d = new Vec3d(((double) RANDOM.nextFloat() - 0.5D) * 0.1D,
+            RANDOM.nextDouble() * 0.1D + 0.1D, 0.0D);
+        vec3d = vec3d.rotateX(-player.getPitch() * modifier);
+        vec3d = vec3d.rotateY(-player.getYaw() * modifier);
+        double d0 = (double) (-RANDOM.nextFloat()) * 0.6D - 0.3D;
+        Vec3d vec3d1 = new Vec3d(((double) RANDOM.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
+        vec3d1 = vec3d1.rotateX(-player.getPitch() * modifier);
+        vec3d1 = vec3d1.rotateY(-player.getYaw() * modifier);
+        vec3d1 = vec3d1.add(player.getX(), player.getEyeY(), player.getZ());
+        world.addParticle(new ItemStackParticleEffect(ParticleTypes.ITEM, stack), vec3d1.x,
+            vec3d1.y, vec3d1.z, vec3d.x, vec3d.y + 0.05D, vec3d.z);
+      }
+      player.playSound(player.getEatSound(stack), 0.5F + 0.5F * (float) RANDOM.nextInt(2),
+          (RANDOM.nextFloat() - RANDOM.nextFloat()) * 0.2F + 1F);
       return ActionResult.PASS;
     });
   }
