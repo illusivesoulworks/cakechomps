e43e7f3bf52588d20c541717f50a11d316fc8a2d
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2025-10-13 23:37:16 -0700

    Update to Minecraft 1.21.3

--- common/src/main/java/com/illusivesoulworks/cakechomps/mixin/CakePipeline.java
+++ common/src/main/java/com/illusivesoulworks/cakechomps/mixin/CakePipeline.java
@@ -2,17 +2,15 @@ package com.illusivesoulworks.cakechomps.mixin;
 
 import java.util.Random;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ItemParticleOption;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.Consumable;
+import net.minecraft.world.item.component.Consumables;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CakeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
 
 public class CakePipeline {
 
@@ -41,28 +39,14 @@ public class CakePipeline {
       if (flag) {
         ItemStack blockStack = this.refStack;
 
-        for (int i = 0; i < 16; ++i) {
-          Vec3 vec3 =
-              new Vec3(((double) RANDOM.nextFloat() - 0.5D) * 0.1D, Math.random() * 0.1D + 0.1D,
-                  0.0D);
-          vec3 = vec3.xRot(-player.getXRot() * ((float) Math.PI / 180F));
-          vec3 = vec3.yRot(-player.getYRot() * ((float) Math.PI / 180F));
-          double d0 = (double) (-RANDOM.nextFloat()) * 0.6D - 0.3D;
-          Vec3 vec31 = new Vec3(((double) RANDOM.nextFloat() - 0.5D) * 0.3D, d0, 0.6D);
-          vec31 = vec31.xRot(-player.getXRot() * ((float) Math.PI / 180F));
-          vec31 = vec31.yRot(-player.getYRot() * ((float) Math.PI / 180F));
-          vec31 = vec31.add(player.getX(), player.getEyeY(), player.getZ());
-          ParticleOptions particle = new ItemParticleOption(ParticleTypes.ITEM, blockStack);
+        if (!blockStack.isEmpty()) {
+          Consumable consumable =
+              blockStack.getOrDefault(DataComponents.CONSUMABLE, Consumables.DEFAULT_FOOD);
 
-          if (player.level() instanceof ServerLevel serverWorld) {
-            serverWorld.sendParticles(particle, vec31.x, vec31.y, vec31.z, 1, vec3.x,
-                vec3.y + 0.05D, vec3.z, 0.0D);
-          } else {
-            level.addParticle(particle, vec31.x, vec31.y, vec31.z, vec3.x, vec3.y + 0.05D, vec3.z);
+          if (consumable.shouldEmitParticlesAndSounds(0)) {
+            consumable.emitParticlesAndSounds(player.getRandom(), player, blockStack, 5);
           }
         }
-        player.playSound(player.getEatingSound(blockStack), 0.5F + 0.5F * (float) RANDOM.nextInt(2),
-            (RANDOM.nextFloat() - RANDOM.nextFloat()) * 0.2F + 1.0F);
       }
     }
     this.lastBiteLevel = -1;
